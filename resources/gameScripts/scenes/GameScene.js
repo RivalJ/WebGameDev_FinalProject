be3975bc8eed2caf86c6705e57aa31d464fc0db2
Pranish Adhikari <156488539+PranishAdhikari@users.noreply.github.com>
2025-08-01 12:57:58 -0400

    burger chnages

--- resources/gameScripts/scenes/GameScene.js
+++ resources/gameScripts/scenes/GameScene.js
@@ -3,7 +3,23 @@ class GameScene extends Phaser.Scene {
   constructor() {
     super("playGame");
   }
+  preload() {
+    this.load.image("normalBurger", "resources/images/normalBurger.png");
+    this.load.image("evilBurger", "resources/images/evilBurger.png");
+  }
+
   create() {
     this.add.text(20, 20, "The game has loaded!");
+
+    this.burgers = [];
+
+    // Example: create a burger at x=200, y=200
+    this.burgers.push(new Burger(this, 200, 200, Math.random() > 0.5));
+  }
+
+  update(time, delta) {
+    for (const burger of this.burgers) {
+      burger.update(delta);
+    }
   }
 }
